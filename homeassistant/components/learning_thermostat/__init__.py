21d2acc7f3aefcf0c1bf947c7771addd3ccb5838
Brian Bouterse <bmbouter@gmail.com>
2020-03-12 19:00:14 -0400

    Started new component

---
+++ homeassistant/components/learning_thermostat/__init__.py
@@ -0,0 +1,10 @@
+
+
+class MyClimateDevice(ClimateDevice):
+    # Implement one of these methods.
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+
+    async def async_set_temperature(self, **kwargs):
+        """Set new target temperature."""
